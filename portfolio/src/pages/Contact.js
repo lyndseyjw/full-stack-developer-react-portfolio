76b3c487be708c509c412ce091acc81554efac18
lyndseyjw <lyndseyjwatson@gmail.com>
2021-10-14 19:01:21 -0700

    created contact form with email validation

--- portfolio/src/pages/Contact.js
+++ portfolio/src/pages/Contact.js
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function Contact() {
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+
+        const form = event.currentTarget;
+
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
     return (
         <section id="contact">
 
-            <h1>CONTACT ME</h1>   
+            <h1> I'd Love to Hear From You! </h1>
 
-            <nav>
-                <ul>
-                    <li>
-                        <a id="number" href="#contact">(832) 687-2196</a>
-
-                        <a href="mailto:lyndseyjwatson@gmail.com">lyndseyjwatson@gmail.com</a>
-
-                        <a href="https://github.com/lyndseyjw" target="_blank" rel="noopener noreferrer">GitHub</a>
-
-                        <a href="https://www.linkedin.com/in/lyndsey-watson-b22478215/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-
-                    </li>
-
-                </ul>
-
-            </nav>
+            <Form noValidate validated={validated} onSubmit={handleSubmit} className="smallerContainer">
+                <Form.Group className="mb-3" controlId="validationCustom01">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        style = {{ backgroundColor: 'whitesmoke' }}
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="validationEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                        required
+                        type="email"
+                        style = {{ backgroundColor: 'whitesmoke' }}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Please enter a valid email
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Send me a Message!</Form.Label>
+                    <Form.Control 
+                        as="textarea" 
+                        rows={5} 
+                        style = {{ backgroundColor: 'whitesmoke' }}
+                    />
+                </Form.Group>
+                <Button variant="light" type="submit">
+                    Submit
+                </Button>
+            </Form>
 
         </section>
     )
